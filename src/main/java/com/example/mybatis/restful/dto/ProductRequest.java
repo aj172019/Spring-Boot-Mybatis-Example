01d2986adb0581aca36fbe936c21c3ea57788fe2
mz01-hanjiseon <hanj1459@cu.ac.kr>
2023-06-26 09:07:11 +0900

    disable csrf

--- src/main/java/com/example/mybatis/restful/dto/ProductRequest.java
+++ src/main/java/com/example/mybatis/restful/dto/ProductRequest.java
@@ -1,10 +1,23 @@
 package com.example.mybatis.restful.dto;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
-public class ProductRequest extends  ProductResponse {
+@Getter
+@Setter
+public class ProductRequest   {
 
+    private Long id;        // 상품의 ID
+    private String name;    // 상품의 이름
+    private String description; // 상품의 설명
+    private double price;   // 상품의 가격
+
+    @Builder
     protected ProductRequest(Long id, String name, String description, double price) {
-        super(id, name, description, price);
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
     }
 }
